1f51dc570f676488a95513ebe9f956faddfb88d6
Bettiana <balle.bettiana@gmail.com>
2017-10-31 10:02:13 +0100

    tablas

--- server/boot/migration.js
+++ server/boot/migration.js
@@ -3,11 +3,11 @@
 module.exports = function(app) {
     //data sources
     var mysqlDs = app.dataSources.mysqlDs;
-
+    if(process.env.AUTOMIGRATE){
     mysqlDs.automigrate(null, function (err) {
         if (err) console.log(err);
         console.log('> Models migrated to tables');
-        
+    
         app.loadFixtures()
         .then(function() {
           console.log('Done!');
@@ -16,4 +16,5 @@ module.exports = function(app) {
           console.log('Errors:', err);
         });
     });
+}
 }
